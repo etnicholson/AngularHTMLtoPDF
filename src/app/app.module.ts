098f93f065fedf9dbfc9f31db074b235f049e2cc
etnicholson@gmail.com <etnicholson@gmail.com>
2019-04-18 10:43:57 -0400

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule }  from '@angular/forms';
 import {MatInputModule} from '@angular/material';
 
 
